92588b09916b601de534e9c46ff02af65dbf6948
Navaneeth Malingan <navneetnivu07@gmail.com>
2020-03-27 17:33:56 +0530

    tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
+        // nivu.me tracking id
         trackingId: "UA-145419540-1",
         head: true,
       }
