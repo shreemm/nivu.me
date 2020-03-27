b4396968223ccf67f3f0f898455567a4030a25ce
Navaneeth Malingan <navneetnivu07@gmail.com>
2020-03-27 17:28:32 +0530

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,8 +52,7 @@ module.exports = {
   {
     resolve: `gatsby-plugin-google-analytics`,
     options: {
-      // The property ID; the tracking code won't be generated without it
-      trackingId: "UA-30027142-1",
+      trackingId: "UA-145419540-1",
       head: true,
     }
   },
